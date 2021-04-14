c948ea474cf38ff20a3b42827bfc846ab2b2fe9e
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-04-14 23:39:09 +0300

    Update 7-User-defined_Functions_&_Function_files.m

--- Matlab_Applications/7-User-defined_Functions_&_Function_files.m
+++ Matlab_Applications/7-User-defined_Functions_&_Function_files.m
@@ -2,4 +2,26 @@
 % function file, and then used like a build-in function
 % In many cases this is a subprogram.
 % Schematically: Input data --> Function File --> Output data
-% Simularities and differences b/n Script filae and Function files
+% Simularities and differences b/n Script filae and Function files:
+%     Script Files          |        Function files
+% --------------------------|-----------------------------
+%  saved with extencipn .m  | saved with extencipn .m
+% --------------------------|-----------------------------
+%            any line       | first line should function definition line
+% --------------------------|-----------------------------
+%  variables are recognised | variables are local
+%   in the Command Window   |
+% --------------------------|-----------------------------
+%  can use variables that   | can accept data through input arguments 
+%  defined in the workspace | and can return data through
+%  -------------------------| output arguments
+%  contain sequence of      |  
+%  MATLAB commands          | 
+%  -------------------------|-----------------------------
+%                           | The name of the file should be 
+%                           | the same as the name of the function
+% --------------------------|-----------------------------
+% A user-defined function is used in the same way as a built-in function.
+% it can be (used) in the Command Window, in a script file, or in 
+% another function.
+% --------------------------------------------------------
