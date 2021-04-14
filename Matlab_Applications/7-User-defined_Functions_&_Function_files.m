c7228ea9468662afd938bb9226fff740bddc9e49
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-04-14 23:04:07 +0300

    Update 7-User-defined_Functions_&_Function_files.m

--- Matlab_Applications/7-User-defined_Functions_&_Function_files.m
+++ Matlab_Applications/7-User-defined_Functions_&_Function_files.m
@@ -1,1 +1,5 @@
-
+% A user-defined function ia MATLAB file that is created by user, saved as 
+% function file, and then used like a build-in function
+% In many cases this is a subprogram.
+% Schematically: Input data --> Function File --> Output data
+% Simularities and differences b/n Script filae and Function files
