ac00d16c0ab029fe48d6026165b89511b35e0be3
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-04-14 23:57:29 +0300

    Update 7-User-defined_Functions_&_Function_files.m

--- Matlab_Applications/7-User-defined_Functions_&_Function_files.m
+++ Matlab_Applications/7-User-defined_Functions_&_Function_files.m
@@ -1,3 +1,8 @@
+clc
+clear all;
+
+x = 3;
+
 % A user-defined function ia MATLAB file that is created by user, saved as 
 % function file, and then used like a build-in function
 % In many cases this is a subprogram.
@@ -25,3 +30,26 @@
 % it can be (used) in the Command Window, in a script file, or in 
 % another function.
 % --------------------------------------------------------
+%
+% 7.8 Anonymous Fucntions
+% User-defined function could be used several times in the complex
+% calculations & large copmuter programs.
+% An anonymous function is a user-defined function that defined and written
+% within the computer code (not in a separate funtion file) and it used in
+% the code. 
+% N.B. >>> Anonymous function can be defined in any part of MATLAB (in the
+% Command Window, in script files, and inside regular user-defined
+% functions). 
+% name = @ (arlist) mathematical expression
+% arlist - a list of input arguments (independent variables)
+% Examples:
+
+
+cube = @(x) x ^ 3
+circle = @(x,y) 16*x^2 + 9*y^2
+parabola = @(x) a*x^2 + b*x + c
+
+
+
+
+
