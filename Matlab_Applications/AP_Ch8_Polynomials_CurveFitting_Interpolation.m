2f9cd9740198c489e789eaf0e763fdddfadd7b3a
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-04-15 10:28:43 +0300

    Update and rename 8-Polynomials_curve-fitting_&_Interpolation.m to AP_Ch8_Polynomials_CurveFitting_Interpolation.m

---
+++ Matlab_Applications/AP_Ch8_Polynomials_CurveFitting_Interpolation.m
@@ -0,0 +1,20 @@
+% AP_Ch8_Polinomials,Curve_Fitting,_and_Interpolation
+% 
+% Polynomials are mathematical expression that are frequently used for
+% problem solving and modeling in science and enginnering. In many cases an
+% equation that is written int he process of solving a problem is a
+% polynomial, and the solution of the problem is the zero of the
+% polynomial. 
+%
+% Curve fitting is a process of finding a fucntion that can be used to
+% model data. The function does not necessarily pass through any of the
+% points, but models the data with the smallest possible error. There are
+% no limitations to the type of the equations that can be used for curve 
+% fitting. Often, however, polynomial, exponential, and power functions 
+% are used.    
+%
+% Interpolation is the process of estimating values between data points. 
+% The simplest kind of interpolation is done by drawing a straight line 
+% between the points. In a more sophistikated interpolation, data from
+% additional points is used. 
+% 
