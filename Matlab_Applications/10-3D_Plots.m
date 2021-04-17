01f7e0c05872d1831de79fe9dfbb6a077f0d6d00
Andrey Pimenov <andrey.pimenov@skolkovotech.ru>
2021-04-17 09:09:36 +0300

    Update 10-3D_Plots.m

--- Matlab_Applications/10-3D_Plots.m
+++ Matlab_Applications/10-3D_Plots.m
@@ -19,3 +19,43 @@ plot3(x,y,z,'k','linewidth',1);
 grid on
 xlabel('x'); ylabel('y'); zlabel('z');
 % ------------------------------------
+
+% 10.2 Mesh and Surface Plots
+% Mesh & surface plots are 3-D plots used the plotting functions of the
+% form z = f(x,y), where x and y are independent variables, z - dependent
+% 1st step create a grid of XY plane that covers the domain of the function
+% 2nd step calculate the value of z at each point of the grid
+% 3rd step create the plot
+% 1st step: creating a grid
+x = -1 :0.1: 3; % vector that divides the domain of x
+y = 1 :0.1: 4;  % vector that divides the domain of y
+[X, Y] = meshgrid(x,y) % X,Y - matrises of x,y coordiunates respectevily
+
+% 2nd step: z values calculations
+Z = X.*Y.^2./(X.^2 + Y.^2)
+
+% 3rs step: makinga mesh and surface plots:
+mesh(X,Y,Z) % or surf (X,Y,Z)
+xlabel('x'); ylabel('y'); zlabel('z');
+grid off % usefull for nice pictures for dissertation
+box on
+
+% Examples:
+x = -3 :0.25 : 3;
+y = -3 :0.25 : 3;
+[X, Y] = meshgrid(x,y);
+
+Z = 1.8.^(-1.5*sqrt(X.^2 + Y.^2)).*cos(0.5*Y).*sin(X);
+% different types of Graphs:
+mesh(X,Y,Z)
+meshz(X,Y,Z)
+meshc(X,Y,Z)
+
+surf(X,Y,Z)
+surfc(X,Y,Z)
+surfl(X,Y,Z)
+
+waterfall(X,Y,Z)
+contour3(X,Y,Z,55) %where n = 55 is the number of countor levels
+contour(X,Y,Z,55) %where n = 55 is the number of countor levels
+xlabel('x'); ylabel('y'); zlabel('z');
